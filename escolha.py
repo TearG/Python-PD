93355f74317ee3b71e884da01e0d2cf375e4ade3
Van Faria <57229209+TearG@users.noreply.github.com>
2024-02-27 00:58:34 +0000

    Aula 05 - Entrada e saída de dados em Python

---
+++ escolha.py
@@ -0,0 +1,3 @@
+#Entrada e saída de dados em Python
+numero = input("Digite um número: ")
+print("Número digitado: ", numero)
---
+++ soma.py
@@ -0,0 +1,4 @@
+#Entrada e saída de dados em Python
+x = int(input("Digite o primeiro número: "))
+y = int(input("Digite o segundo número: "))
+print("O resultado da soma é: ", x + y)
